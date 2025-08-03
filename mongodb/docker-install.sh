4675c4d297196addaa471df72a7c329c02514091
labcabrera <lab.cabrera@gmail.com>
2025-08-04 01:16:12 +0200

    Added kafka docker compose for local development. Updated local mongo create docker image script. Updated readme.

--- mongodb/docker-install.sh
+++ mongodb/docker-install.sh
@@ -3,9 +3,14 @@
 MONGO_VERSION=8.0
 ADMIN_USER=admin
 ADMIN_PASS=admin
+NAME="rmu-mongo"
+
+docker stop ${NAME} 2>/dev/null || true
+docker rm ${NAME} 2>/dev/null || true
 
 docker run -d \
-  --name mongo-local \
+  --name ${NAME} \
+  --network rmu-network \
   -p 27017:27017 \
   -e MONGO_INITDB_ROOT_USERNAME=${ADMIN_USER} \
   -e MONGO_INITDB_ROOT_PASSWORD=${ADMIN_PASS} \
